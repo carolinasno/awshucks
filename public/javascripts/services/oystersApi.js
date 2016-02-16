076b3a83b1e936d3069feb2421096e5f8b089c77
Carolina No <carolina.s.no@gmail.com>
2016-02-16 11:48:42 -0500

    fixes hardcode localhost

--- public/javascripts/services/oystersApi.js
+++ public/javascripts/services/oystersApi.js
@@ -3,7 +3,7 @@ var api = angular.module('oystersApiFactory', []);
 api.factory('oystersApi', ['$http', function($http){
 
   var oysterInterface = {};
-  var baseUrl = 'http://localhost:8080/api/oysters/';
+  var baseUrl = '/api/oysters/';
 
   oysterInterface.allOysters = function(){
     return $http.get(baseUrl);
